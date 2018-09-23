4e35054427997527094a21139156150e3f6d4c41
Gennad-r <gennad.r@gmail.com>
2018-09-24 01:31:32 +0300

    homepage draft fix

--- app/js/common.js
+++ app/js/common.js
@@ -10,7 +10,6 @@ $(function() {
 		$.each($('.about-company .about-company__plate'), function(i, el){
 			$(el).removeClass('active-plate');
 		})
-			console.log($(e.target).parent('.about-company__plate'));
 			$(e.target).parent('.about-company__plate').addClass('active-plate');
 			var dataAttr = $(e.target).parent('.about-company__plate').attr('data-num');
 			$('.owl-carousel.pict-slider').trigger('to.owl.carousel', dataAttr);
