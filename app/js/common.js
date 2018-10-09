abb77cf9851942fbc72341c511c2c345c2f5dad6
Gennad-r <gennad.r@gmail.com>
2018-10-09 10:15:20 +0300

    third menu level, slider navigation, plates, accordion removed

--- app/js/common.js
+++ app/js/common.js
@@ -1,17 +1,11 @@
 $(function() {
 	$('.owl-carousel.pict-slider').owlCarousel({
 		'items': 1,
-		'loop': true
+		'loop': true,
+		'nav': true,
+		'navText': ['<span class="icon-arr-left"></span>', '<span class="icon-arr-right"></span>']
 	});
 
 
-	$('.about-company').click(function (e) {
-		e.stopPropagation();
-		$.each($('.about-company .about-company__plate'), function(i, el){
-			$(el).removeClass('active-plate');
-		})
-			$(e.target).parent('.about-company__plate').addClass('active-plate');
-			var dataAttr = $(e.target).parent('.about-company__plate').attr('data-num');
-			$('.owl-carousel.pict-slider').trigger('to.owl.carousel', dataAttr);
-	})
+	$('body>a').hide();
 });
