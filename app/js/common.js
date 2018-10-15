3371b0ed8599e8471fc00fdcc49b4d07303b9310
Gennad-r <gennad.r@gmail.com>
2018-10-15 23:22:04 +0300

    holidays for serg

--- app/js/common.js
+++ app/js/common.js
@@ -6,6 +6,25 @@ $(function() {
 		'navText': ['<span class="icon-arr-left"></span>', '<span class="icon-arr-right"></span>']
 	});
 
-
+	// garbage cleaner )
 	$('body>a').hide();
+	// garbage cleaner )
+
+
+
 });
+
+
+
+//complex solutions
+var solutionPlates = $('.complex-solutions .solution'),
+	solutionTextBlocks = $('.complex-solutions .solution__text-block');
+$(solutionTextBlocks[0]).show();
+solutionPlates.on('click', function (e) {
+	e.preventDefault();
+	solutionPlates.removeClass('active');
+	solutionTextBlocks.hide();
+	$(this).addClass('active');
+	$(solutionTextBlocks[$(this).data('order')]).fadeIn();
+})
+
