67b9258b6f5e3e602d019edec8e5eeb4ea2999c0
Gennad-r <gennad.r@gmail.com>
2018-10-19 09:36:52 +0300

    small js changes, to top button

--- app/js/common.js
+++ app/js/common.js
@@ -2,6 +2,7 @@ $(function() {
 	$('.owl-carousel.pict-slider').owlCarousel({
 		'items': 1,
 		'loop': true,
+		'autoplay': true,
 		'nav': true,
 		'navText': ['<span class="icon-arr-left"></span>', '<span class="icon-arr-right"></span>']
 	});
@@ -28,3 +29,18 @@ solutionPlates.on('click', function (e) {
 	$(solutionTextBlocks[$(this).data('order')]).fadeIn();
 })
 
+// to top
+$('.go-top').click(function () {
+	$('html, body').stop().animate({
+					scrollTop: 0
+				}, 600);
+})
+$(window).on('scroll', (function() {
+		var scrollDistance = $(window).scrollTop();
+		if ( window.scrollY >= window.innerHeight * 0.8 ) {
+			$('.go-top').addClass('s-scrolled');
+		} else {
+			$('.go-top').removeClass('s-scrolled');
+		};
+
+	}));
